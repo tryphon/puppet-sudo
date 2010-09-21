5cc9e8d9c8befbf72fd2d33f5f3d45b22713457f
Alban Peignier <alban.peignier@free.fr>
2010-09-21 13:13:07 +0200

    Created from Tryphon project

---
+++ manifests/init.pp
@@ -0,0 +1,32 @@
+class sudo {
+
+  package { sudo: ensure => installed }
+
+  define user_line($line) {
+    line { "sudo-$name":
+      file => "/etc/sudoers",
+      line => $line
+    }
+  }
+
+  user_line { root: line => "root	ALL=(ALL) ALL" }
+  user_line { adm: line => "%adm	ALL=(ALL) ALL" }
+
+}
+
+class sudo::ssh_agent_auth {
+
+  package { libpam-ssh-agent: }
+
+  file { "/etc/pam.d/sudo":
+    source => "puppet:///sudo/sudo.pam",
+    require => Package[libpam-ssh-agent]
+  }
+
+  line { "sudoers_env_keep_ssh_auth_sock":
+    file => "/etc/sudoers",
+    line => "Defaults env_keep += SSH_AUTH_SOCK",
+    require => Package[libpam-ssh-agent]
+  }
+
+}
